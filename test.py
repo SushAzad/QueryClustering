fa41d339bec03b2ef6d8889d6996c3955e3d59a6
sushazad <azad.sushmita@gmail.com>
2020-04-06 18:11:38 -0500

    Update and Delete

--- test.py
+++ test.py
@@ -10,3 +10,19 @@ db.create_question("test question insert")
 
 print("After Insert")
 print(db.read_all_questions())
+
+print("Testing Update. Before update:")
+print(db.read_all_questions())
+
+qid = input("Enter question ID to be updated: ")
+q = input("Enter new value for question: ")
+db.update_question_by_id(qid, q)
+print("After update")
+print(db.read_all_questions())
+
+print("Before Delete")
+print(db.read_all_questions())
+qid = input("Enter question ID to be deleted (Questions Table): ")
+db.delete_question_by_id(qid)
+print("After Delete")
+print(db.read_all_questions())
