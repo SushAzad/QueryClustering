a5e6ee27fa6d456a3799984f031a6954b85600b3
sushazad <azad.sushmita@gmail.com>
2020-05-05 12:19:25 -0700

    Housekeeping

--- app-engine/main.py
+++ app-engine/main.py
@@ -76,10 +76,10 @@ def callCluster():
     # features = clus.Features() 
     kwargs=dict(name=None, n_clusters=None, dist=None, link=None, 
     q_type=None, emb_type=None, printing=None)
-    if 'algo' in request.args:
-        algo = request.args.get('algo')
-        kwargs['name']=algo
-        print("algo", algo)
+    if 'name' in request.args:
+        name = request.args.get('name')
+        kwargs['name']=name
+        print("name", name)
     if 'n_clusters' in request.args:
         n_clusters = int(request.args.get('n_clusters'))
         kwargs['n_clusters']=n_clusters
